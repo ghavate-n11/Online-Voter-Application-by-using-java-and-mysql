8c9f4a4facb788885c718818ea0f5f8b5029d512
Nilesh Ghavate <nileshghavate11@gmail.com>
2025-07-12 23:56:26 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,13 +1,59 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Online Voting System</title>
-<script src="jscript/validation.js" type="text/javascript"></script>
+  <meta charset="UTF-8">
+  <title>Online Voting System</title>
+  <link rel="stylesheet" href="styles.css"> <!-- Optional external CSS -->
+  <script src="jscript/validation.js" defer></script>
+  <style>
+    body {
+      background-color: #EBE9E9;
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+    header {
+      background-color: #333;
+      padding: 15px 0;
+      text-align: center;
+    }
+    header h1 {
+      color: white;
+      margin: 0;
+    }
+    nav {
+      margin: 20px auto;
+      text-align: center;
+    }
+    nav a {
+      text-decoration: none;
+      color: #333;
+      font-size: 20px;
+      margin: 0 15px;
+      padding: 8px 16px;
+      border: 1px solid #333;
+      border-radius: 6px;
+      background-color: #fff;
+      transition: background-color 0.3s, color 0.3s;
+    }
+    nav a:hover {
+      background-color: #333;
+      color: #fff;
+    }
+  </style>
 </head>
-<body bgcolor="#EBE9E9">
-<marquee>Welcome To Online Voting Application Coded By NILESH GHAVATE</marquee>
-<center><font size='8' >
-<a href="index.php">Home</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="register.php">Register</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="login.php">Login</a></font></center> 
-<br>
-<br>
+<body>
+
+  <header>
+    <h1>Welcome to the Online Voting System</h1>
+    <p style="color:white;">Coded by Nilesh Ghavate</p>
+  </header>
+
+  <nav>
+    <a href="index.php">Home</a>
+    <a href="register.php">Register</a>
+    <a href="login.php">Login</a>
+  </nav>
+
+</body>
+</html>
